2a169a281fbbd9b69d19332902dd443a7f5065c9
kerviin <kervinterencereyes17@gmail.com>
2021-06-08 16:43:19 +0800

    Push Try

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Copyright &copy; JUANIFY. </strong>
+     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.5
     </div>
