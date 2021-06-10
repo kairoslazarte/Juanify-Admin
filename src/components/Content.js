a8ae345527c9e32e51bd948c597d9aae46e430f8
Jp <garciajp935@gmail.com>
2021-06-10 20:09:25 +0800

    minor changes like adding table and hover

--- src/components/Content.js
+++ src/components/Content.js
@@ -25,45 +25,45 @@ export default function Content() {
         <div className="container-fluid">
           <div class="row mb-3">
             <div class="col-xl-3 col-lg-6">
-              <div class="card card-inverse card-success">
+              <div class="card card-inverse card-success cardEffects ">
                 <div class="card-block bg-success">
                   <div class="rotate">
                     <i class="fa fa-user fa-5x"></i>
                   </div>
-                  <h6 class="text-uppercase">Users</h6>
+                  <h6 class="text-uppercase">Received</h6>
                   <h1 class="display-1">134</h1>
                 </div>
               </div>
             </div>
             <div class="col-xl-3 col-lg-6">
-              <div class="card card-inverse card-danger">
+              <div class="card card-inverse card-danger cardEffects">
                 <div class="card-block bg-danger">
                   <div class="rotate">
                     <i class="fa fa-list fa-4x"></i>
                   </div>
-                  <h6 class="text-uppercase">Posts</h6>
+                  <h6 class="text-uppercase">Processing</h6>
                   <h1 class="display-1">87</h1>
                 </div>
               </div>
             </div>
             <div class="col-xl-3 col-lg-6">
-              <div class="card card-inverse card-info">
+              <div class="card card-inverse card-info cardEffects">
                 <div class="card-block bg-info">
                   <div class="rotate">
                     <i class="fa fa-twitter fa-5x"></i>
                   </div>
-                  <h6 class="text-uppercase">Tweets</h6>
+                  <h6 class="text-uppercase">Delivered</h6>
                   <h1 class="display-1">125</h1>
                 </div>
               </div>
             </div>
             <div class="col-xl-3 col-lg-6">
-              <div class="card card-inverse card-warning">
+              <div class="card card-inverse card-warning cardEffects">
                 <div class="card-block bg-warning">
                   <div class="rotate">
                     <i class="fa fa-share fa-5x"></i>
                   </div>
-                  <h6 class="text-uppercase">Shares</h6>
+                  <h6 class="text-uppercase">Cancelled</h6>
                   <h1 class="display-1">36</h1>
                 </div>
               </div>
